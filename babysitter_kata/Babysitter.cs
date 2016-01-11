ed751536205e605c4d5bbe0f5065a88411b71981
SirHoneyBiscuit <smith.benjamin.joseph@gmail.com>
2016-01-10 19:49:58 -0500

    Refactor first test. Write Second test.

--- babysitter_kata/Babysitter.cs
+++ babysitter_kata/Babysitter.cs
@@ -8,15 +8,18 @@ namespace babysitter_kata
 {
     public class Babysitter
     {
-        public Babysitter()
-        {
+        private int totalFee = 0;
 
-        }
+        public Babysitter(){}
 
         public string fee()
         {
-            return "$0";
+            return "$" + totalFee.ToString();
         }
 
+        public string job(DateTime _startTime, DateTime _endTime, DateTime _bedTime)
+        {
+            return fee();
+        }
     }
 }
--- babysitter_kataTest/BabysitterUnitTests.cs
+++ babysitter_kataTest/BabysitterUnitTests.cs
@@ -8,6 +8,15 @@ namespace babysitter_kataTest
     public class BabysitterUnitTests
     {
 
+        /*  
+         * The babysitter class contains a job function 
+         *  It should take 3 values
+         *  1. Start time
+         *  2. End Time
+         *  3. Bed Time
+         *  Should return the fee for that job 
+         */
+
         [TestInitialize]
         public void setupUnitTest()
         {}
@@ -21,6 +30,16 @@ namespace babysitter_kataTest
         {
             Babysitter sitter = new Babysitter();
             Assert.AreEqual("$0", sitter.fee());
+        }
+
+        [TestMethod]
+        public void emptyTimesMeanNoJob()
+        {
+            Babysitter sitter = new Babysitter();
+            DateTime startTime = new DateTime();
+            DateTime endTime = new DateTime();
+            DateTime bedTime = new DateTime();
+            Assert.AreEqual("$0", sitter.job(startTime, endTime, bedTime));
         }
     }
 }
