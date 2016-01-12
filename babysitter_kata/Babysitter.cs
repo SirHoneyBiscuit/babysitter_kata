52e52f1e257359877127131a837fde6ea0e33f84
SirHoneyBiscuit <smith.benjamin.joseph@gmail.com>
2016-01-11 19:55:37 -0500

    Split out notion of time math into it's own Job class. The babysitter and job should be agnostic of each other.

--- babysitter_kata/Babysitter.cs
+++ babysitter_kata/Babysitter.cs
@@ -22,42 +22,13 @@ namespace babysitter_kata
 
         public string job(DateTime _startTime, DateTime _endTime, DateTime _bedTime)
         {
-            var midnight = _startTime.Subtract(_startTime.TimeOfDay);
-            // If we start after midnight, we want the midnight of the next day
-            // This will allow us to use the built in comparison functions
-            if (_startTime.Hour > 4)
-                midnight = midnight.AddDays(1);
+            Job tonightsJob = new Job(_startTime,_endTime, _bedTime);
 
-            var normalRateHours = getNormalRateHours(_startTime, _endTime, midnight);
-            var afterMidnightHours = getAfterMidnightHours(_startTime, _endTime, midnight);
+            var normalRateHours = tonightsJob.getNormalRateHours();
+            var afterMidnightHours = tonightsJob.getAfterMidnightHours();
             TotalFee = (normalRateHours * StartToBedOrMidnightRate) 
                      + (afterMidnightHours * MidnightToEndRate);             
             return fee();
-        }
-
-        public int getNormalRateHours(DateTime _startTime, DateTime _endTime, DateTime _midnight)
-        {
-            if(_endTime <= _startTime || _startTime > _midnight)
-            {
-                return 0;
-            }
-            if (_endTime < _midnight)
-                return _endTime.Hour - _startTime.Hour;
-            // Since the midnight hour is 0, we need to subtract the start time from 24 (which, in this case, is also midnight)
-            return 24 - _startTime.Hour;
-        }
-
-        public int getAfterMidnightHours(DateTime _startTime, DateTime _endTime, DateTime _midnight)
-        {
-            if(_endTime < _midnight)
-            {
-                return 0;
-            }
-            if(_startTime > _midnight)
-            {
-                return _endTime.Hour - _startTime.Hour;
-            }
-            return _endTime.Hour;
         }
     }
 }
---
+++ babysitter_kata/Job.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace babysitter_kata
+{
+    // The start, end, and beds times are not really a charactic of a baby sitter, but rather the job itself.
+    // It makes sense to split this out into it's own class.
+    public class Job
+    {
+        DateTime StartTime;
+        DateTime EndTime;
+        DateTime BedTime;
+        DateTime Midnight;
+        private const int MinStartTime   = 17;
+        private const int MaxEndTime     = 4;
+        private const int MidnightAsHour = 24;
+
+        public Job(DateTime _startTime, DateTime _endTime, DateTime _bedTime)
+        {
+            StartTime = _startTime;
+            EndTime = _endTime;
+            BedTime = _bedTime;
+
+            // If we start after midnight, we want the midnight of the next day
+            // This will allow us to use the built in comparison operators
+            Midnight = StartTime.Subtract(_startTime.TimeOfDay);
+            if (StartTime.Hour > MaxEndTime)
+                Midnight = Midnight.AddDays(1);
+        }
+
+        public int getNormalRateHours()
+        {
+            if (EndTime <= StartTime || StartTime > Midnight)
+            {
+                return 0;
+            }
+            if (EndTime < Midnight)
+                return EndTime.Hour - StartTime.Hour;
+            // Since the midnight hour is 0, we need to subtract the start time from 24 (which, in this case, is also midnight)
+            return MidnightAsHour - StartTime.Hour;
+        }
+
+        public int getAfterMidnightHours()
+        {
+            if (EndTime < Midnight)
+            {
+                return 0;
+            }
+            if (StartTime > Midnight)
+            {
+                return EndTime.Hour - StartTime.Hour;
+            }
+            return EndTime.Hour;
+        }
+    }
+}
