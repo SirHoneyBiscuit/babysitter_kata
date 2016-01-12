4a1e11321cd4e38b8368864a907f394471f0ecfa
SirHoneyBiscuit <smith.benjamin.joseph@gmail.com>
2016-01-11 21:54:30 -0500

    Comments and added readme.

--- babysitter_kata/Babysitter.cs
+++ babysitter_kata/Babysitter.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿/*
+Project: Babysitter_Kata
+Author: Benjamin Smith
+Date: 1/11/2016
+*/
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,17 +21,33 @@ namespace babysitter_kata
 
         public Babysitter(){}
 
+        /// <summary>
+        /// Returns the total expected fee for the job, formatted as currency
+        /// </summary>
+        /// <returns></returns>
         public string fee()
         {
             return String.Format("{0:C}", TotalFee);
         }
 
+        /// <summary>
+        /// Returns the total expected fee for the passed DateTime values
+        /// </summary>
+        /// <param name="_startTime"></param>
+        /// <param name="_endTime"></param>
+        /// <param name="_bedTime"></param>
+        /// <returns></returns>
         public string job(DateTime _startTime, DateTime _endTime, DateTime _bedTime)
         {
             Job tonightsJob = new Job(_startTime, _endTime, _bedTime);
             return job(tonightsJob);
         }
 
+        /// <summary>
+        /// Returns the total expected fee for the job passed
+        /// </summary>
+        /// <param name="_newJob"></param>
+        /// <returns></returns>
         public string job(Job _newJob)
         {
             if (_newJob.hasValidHours())
